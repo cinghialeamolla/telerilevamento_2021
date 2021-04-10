a84b36f7533d1c3bdfa26eb69a3d67d2d1069fd4
cinghialeamolla <80597231+cinghialeamolla@users.noreply.github.com>
2021-04-10 23:49:01 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -23,7 +23,7 @@ plot(lst_2005)
 plot(lst_2010)
 plot(lst_2015)
 
-#questa funzione crea una lista di file con punti in comune nel nome
+#questa funzione crea una lista di file con punti in comune nel nome (list)
 rlist <- list.files(pattern="lst")
 rlist
 #questa funzione applica uno stesso comando a una lista di file
@@ -31,9 +31,31 @@ import <- lapply(rlist,raster)
 import
 #questa funzione crea un blocco di file raster uniti insieme in un solo file
 TGr <- stack(import)
+TGr
 #plotto il gruppo di file uniti
 plot(TGr)
 #plot con sovrapposizione delle 3 immagini(2000,2005,2010)
 plotRGB(TGr, 1, 2, 3, stretch="lin")
 #plot con sovrapposizione delle 3 immagini(2005,2010,2015)
 plotRGB(TGr, 2, 3, 4, stretch="lin")
+
+### DAY 2
+library(raster)
+library(rasterVis)
+setwd("C:/lab/greenland")
+
+rlist <- list.files(pattern="lst")
+rlist
+import <- lapply(rlist,raster)
+import
+TGr <- stack(import)
+TGr
+#plottaggio tutto insieme
+levelplot(TGr)
+#plottaggio singola immagine
+levelplot(TGr$lst_2000)
+
+cl<-colorRampPalette(c("blue","light blue","pink","red"))(100)
+
+
+
