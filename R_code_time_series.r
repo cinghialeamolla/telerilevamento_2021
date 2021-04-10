bffd3ff3d2e74d649d755d53907924c97d384d0e
cinghialeamolla <80597231+cinghialeamolla@users.noreply.github.com>
2021-04-10 20:22:40 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -1,5 +1,39 @@
-#Analisi serie temporali
+#Analisi serie multitemporale di dati satellitari
 #aumento delle temperature in Groenlandia
 #dati e codice da Emanuela Cosma (tesista)
 
 library(raster)
+setwd("C:/lab/greenland")
+
+#funzione per importare i singoli dati
+lst_2000 <- raster("lst_2000.tif")
+#plot del I dataset
+plot(lst_2000)
+
+#importazione dei 4 file
+lst_2000 <- raster("lst_2000.tif")
+lst_2005 <- raster("lst_2005.tif")
+lst_2010 <- raster("lst_2010.tif")
+lst_2015 <- raster("lst_2015.tif")
+
+#panel con i plottaggi delle 4 immagini insieme
+par(mfrow=c(2,2))
+plot(lst_2000)
+plot(lst_2005)
+plot(lst_2010)
+plot(lst_2015)
+
+#questa funzione crea una lista di file con punti in comune nel nome
+rlist <- list.files(pattern="lst")
+rlist
+#questa funzione applica uno stesso comando a una lista di file
+import <- lapply(rlist,raster)
+import
+#questa funzione crea un blocco di file raster uniti insieme in un solo file
+TGr <- stack(import)
+#plotto il gruppo di file uniti
+plot(TGr)
+#plot con sovrapposizione delle 3 immagini(2000,2005,2010)
+plotRGB(TGr, 1, 2, 3, stretch="lin")
+#plot con sovrapposizione delle 3 immagini(2005,2010,2015)
+plotRGB(TGr, 2, 3, 4, stretch="lin")
