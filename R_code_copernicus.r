621cd46da13b6f8eb945bbdc00f0393f4fbdb7c8
cinghialeamolla <80597231+cinghialeamolla@users.noreply.github.com>
2021-04-15 23:20:17 +0200

    Update R_code_copernicus.r

--- R_code_copernicus.r
+++ R_code_copernicus.r
@@ -5,4 +5,17 @@
 library(raster)
 library(ncdf4)
 
-setwd()
+setwd("C:/lab/")
+
+#questa funzione (rinomina e) carica il dataset
+fcover <- raster("FCOVER_global.nc")
+fcover
+
+clb <- colorRampPalette(c('yellow','light green','green', 'dark green'))(100) 
+plot(fcover, col=clb)
+
+#questa funzione diminuisce il numero di pixel, ricampionamento bilineare
+fcoverres <- aggregate(fcover, fact=50)
+fcoverres
+plot(fcoverres)
+
