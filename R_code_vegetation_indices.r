c72da15a6765faecd9667dfe9398c64567dfd268
cinghialeamolla <80597231+cinghialeamolla@users.noreply.github.com>
2021-05-05 09:42:19 +0200

    Update R_code_vegetation_indices.r

--- R_code_vegetation_indices.r
+++ R_code_vegetation_indices.r
@@ -1,6 +1,6 @@
 #R_code_vegetation_indices.r
 
-library(raster)
+library(raster) #require(raster)
 setwd("C:/lab/") # Windows
 #questa funzione carica un'immagine su R
 defor1 <- brick("defor1.jpg")
@@ -11,3 +11,14 @@ defor2 <- brick("defor2.jpg")
 par(mfrow=c(2,1))
 plotRGB(defor1, r=1, g=2, b=3, stretch="Lin")
 plotRGB(defor2, r=1, g=2, b=3, stretch="Lin")
+
+### DAY 2
+
+#questa funzione calcola il DVI della prima immagine
+dvi1 <- defor1$defor1.1 - defor1$defor1.2
+
+cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100) # specifying a color scheme
+plot(dvi1, col=cl)
+
+
+
