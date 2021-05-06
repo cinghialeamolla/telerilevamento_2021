682b8e4baeb9e1e58efc6b014cbc914b27691b54
cinghialeamolla <80597231+cinghialeamolla@users.noreply.github.com>
2021-05-06 22:03:50 +0200

    Update R_code_vegetation_indices.r

--- R_code_vegetation_indices.r
+++ R_code_vegetation_indices.r
@@ -16,9 +16,23 @@ plotRGB(defor2, r=1, g=2, b=3, stretch="Lin")
 
 #questa funzione calcola il DVI della prima immagine
 dvi1 <- defor1$defor1.1 - defor1$defor1.2
-
+#plot DVI1
 cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100) # specifying a color scheme
 plot(dvi1, col=cl)
+plot(dvi1, col=cl, main="DVI at time 1")
 
+#calcolo del DVI 2
+dvi2 <- defor2$defor2.1 - defor2$defor2.2
+#plot DVI2
+plot(dvi2, col=cl, main="DVI at time 2")
+
+par(mfrow=c(2,1))
+plot(dvi1, col=cl, main="DVI at time 1")
+plot(dvi2, col=cl, main="DVI at time 2")
+
+#DVI1-DVI2
+difdvi<- dvi1 - dvi2
+cld <- colorRampPalette(c('blue','white','red'))(100) 
+plot(difdvi, col=cld)
 
 
